6afbafdbfb851c5b89afb6acba750825c1ed8881
Кадралиева Аделина <adel.kadralieva@gmail.com>
2026-09-16 18:17:43 +0300

    Lab01:systeminfo investigations

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
-﻿Console.WriteLine($"Компьютер: {Environment.MachineName}");
-Console.WriteLine($"Пользователь: {Environment.UserName}");
-Console.WriteLine($"Дата и время: {DateTime.Now:dd.MM.yyyy HH:mm}");
+﻿// Console.WriteLine($"Компьютер: {Environment.MachineName}");
+// Console.WriteLine($"Пользователь: {Environment.UserName}");
+// Console.WriteLine($"Дата и время: {DateTime.Now:dd.MM.yyyy HH:mm}");
+
+
+// Console.WriteLine();
+// Console.WriteLine($"OC: {Environment.OSVersion}");
+// Console.WriteLine($"64-битная OC: {Environment.Is64BitOperatingSystem}");
+
+// Console.WriteLine($"Логических процессоров: {Environment.ProcessorCount}");
+
+Console.WriteLine($"WorkingSet: {Environment.WorkingSet} байт");
+Console.WriteLine($"WorkingSet: {Environment.WorkingSet / 1024 / 1024} МБ");
